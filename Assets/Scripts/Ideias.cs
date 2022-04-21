8199b168ee95fd5660c061af30f29f11def76a1d
Lauro Henrique Santana de Freitas <lauroh.sfreitas@gmail.com>
2022-04-21 20:58:03 -0300

    Cenário de Arte

---
+++ Assets/Scripts/Ideias.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ideias : MonoBehaviour
+{
+    //Potais o que são e para que servem -
+    //Portais levam para locais/outros jogos/features que desenvolvi para portifólip
+    //Portais podem ser feitos como scriptable objects posteriormente 
+        //portais precisam de - Uma cena de testino 
+        //portais precisam receber - uma cena anterior 
+        //portais poderam precisar de dados para inicialização de scenas 
+
+}
---
+++ Assets/Scripts/Portais/PortaisManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortaisManager : MonoBehaviour
+{
+    //Precisa de um script de load/Carregamento
+    public string Scene;
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(Scene);
+        }
+    }
+
+}
